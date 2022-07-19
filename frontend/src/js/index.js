1030747e130f39b137691e012be0135a0ab1387d
Thayssa Apolinário <thayssaoliveira55@gmail.com>
2022-07-18 21:29:44 -0300

    ajustes no CSS e modal de tarefa

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -9,8 +9,6 @@ const tarefas = [{
     titulo: "tarefas"
 }]
 
-let edit = false;
-
 function getTarefas() {
 
 }
@@ -29,6 +27,7 @@ function adcTarefa() {
         li.classList.add("cx-tarefa");
     
         var div1 = document.createElement("div");
+        div1.classList.add("nome-tarefa");
     
         var h2 = document.createElement("h2");
         h2.innerHTML = tarefas[i].titulo
@@ -39,6 +38,7 @@ function adcTarefa() {
         div1.appendChild(p);
     
         var div2 = document.createElement("div");
+        div2.classList.add("botoes-tarefa")
     
         var btnEditar = document.createElement("button");
         btnEditar.classList.add("btn-editar");
@@ -58,31 +58,6 @@ function adcTarefa() {
         ul[0].appendChild(li)   
     }
 }
-
-function abrirModal() {
-    edit = true;
-    let modal = document.querySelector(".modal")
-
-    if (edit) {
-        modal.style.display = 'block';
-        document.body.style.background = "rgba(0,0,0,0.1)";
-    } else {
-        modal.style.display = "none";
-        document.body.style.background = "#FFF";
-    }
-};
-
-function fecharModal() {
-    let modal = document.querySelector(".modal")
-
-    if (!edit) {
-        modal.style.display = "none";
-        document.body.style.background = "#FFF";
-    } else {
-        modal.style.display = "none";
-        document.body.style.background = "#FFF";
-    }
-};
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Página carregada!")
---
+++ frontend/src/js/modal.js
@@ -0,0 +1,36 @@
+let edit = false;
+
+let openModal = document.getElementsByClassName('nome-tarefa');
+console.log(openModal)
+
+for(let i = 0; i < openModal.length; i++) {
+    openModal[i].addEventListener("click", () => {
+        console.log("clickou!")
+        abrirModal()
+    });    
+}
+
+function abrirModal() {
+    edit = true;
+    let modal = document.querySelector(".modal");
+
+    if (edit) {
+        modal.style.display = 'block';
+        document.body.style.background = "rgba(0,0,0,0.1)";
+    } else {
+        modal.style.display = "none";
+        document.body.style.background = "#FFF";
+    };
+};
+
+function fecharModal() {
+    let modal = document.querySelector(".modal");
+
+    if (!edit) {
+        modal.style.display = "none";
+        document.body.style.background = "#FFF";
+    } else {
+        modal.style.display = "none";
+        document.body.style.background = "#FFF";
+    };
+};
