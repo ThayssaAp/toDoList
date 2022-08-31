2e26b93e10748b3d10d1eea37e4078ba25fa4ed9
Bia-source <bia_ferreirads@yahoo.com>
2022-08-31 13:03:35 -0300

    teste dos servicos de get

--- backend/src/tests/test.spec.ts
+++ backend/src/tests/test.spec.ts
@@ -1,7 +1,8 @@
 import {describe, expect, test} from "@jest/globals";
 import { CriandoTarefasService } from "../services/criandoTarefas.service";
 import { DeletandoTarefasService } from "../services/deletandoTarefas.service";
-
+import { PegandoTarefaByPkService } from "../services/pegandoTarefaByPk.service";
+import { PegandoTarefasService } from "../services/pegandoTarefas.service";
 
 describe("Criar uma tarefa", ()=>{
     test("Espera-se poder criar uma tarefa", async ()=>{
@@ -10,8 +11,8 @@ describe("Criar uma tarefa", ()=>{
             "titulo": "jest",
             "descricao": "testando criacao de tarefa"
         }
-        const tarefa = await service.execute(tarefaTeste);
-        expect(tarefa).toHaveProperty('id');
+        const result = await service.execute(tarefaTeste);
+        expect(result).toHaveProperty('id');
     });
 });
 
@@ -26,10 +27,34 @@ describe("Deletar uma tarefa", ()=>{
         });
         tarefa = tarefaCriada;
     })
-    test("espera-se poder deletar uma tarefa", async ()=>{
-        
+    test("Espera-se poder deletar uma tarefa", async ()=>{   
         const serviceDeletando = new DeletandoTarefasService();  
         const result = await serviceDeletando.execute(tarefa.id);
         expect(result).toHaveProperty('id');
     })
-})
+});
+
+describe("Pegar tarefas", ()=>{
+    test("Espera-se retornar todas as tarefas", async ()=>{
+       const servicePegandoTarefa = new PegandoTarefasService();
+       const result = await servicePegandoTarefa.execute();
+       expect(result).not.toHaveProperty('message');
+    })
+});
+
+describe("Pegar tarefa por id", ()=>{
+    let tarefa: TarefasModelGlobal;
+    beforeAll(async ()=>{
+        const serviceCriando = new CriandoTarefasService();
+        const tarefaCriada: TarefasModelGlobal = await serviceCriando.execute({
+            titulo: "teste jest delete", 
+            descricao: "testando o servico delete pelo jest"
+        });
+        tarefa = tarefaCriada;
+    });
+    test("Espera-se uma tarefa", async ()=>{
+        const servicePegandoTarefa = new PegandoTarefaByPkService();
+        const result = await servicePegandoTarefa.execute(tarefa.id);
+        expect(result).toHaveProperty('id');
+    })
+});
